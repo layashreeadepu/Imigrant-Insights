e37ce24a522b719687771e058cbd1ece35c04083
Laya <layashreeadepu@gmail.com>
2025-04-06 16:32:44 -0400

    Added comments to main sprint for merging files

--- Mergefn/main_script.py
+++ Mergefn/main_script.py
@@ -1,13 +1,14 @@
 # main_script.py
 import sys
 import os
+#Merger function file path
 sys.path.append(os.path.abspath("C:/Users/layas/OneDrive/Desktop/Layashree documents/NEU Cources/Intro to Programming in DS/Mergefn/immigration"))
 from immigration.merger import load_and_clean_data
 
+#File path of xls file for datafile from year 2003 - 2023
 input_folder = "C:/Users/layas/OneDrive/Desktop/Layashree documents/NEU Cources/Intro to Programming in DS/"
 final_df = load_and_clean_data(input_folder)
 
-# Now you can use final_df as you like 
+#Print and export merged file
 print(final_df.head())
 final_df.to_excel("C:/Users/layas/OneDrive/Desktop/Layashree documents/NEU Cources/Intro to Programming in DS/Merged files/cleaned_data.xlsx", index=False)
-
